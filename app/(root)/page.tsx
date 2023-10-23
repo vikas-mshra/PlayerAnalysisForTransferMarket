5d609e48379a00e81a328f4d1a7c827d0554b16d
Vikas Mishra <vikas.mishra0796@gmail.com>
2023-10-23 10:16:55 -0700

    Fetching top 50 players based on their market value

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,54 @@
 import { fetchPlayerDetails } from "@/lib/actions/player.actions";
 
-export default async function Home() {
+interface Player {
+  player_id: number;
+  first_name: string;
+  last_name: string;
+  name: string;
+  last_season: number;
+  current_club_id: number;
+  player_code: string;
+  country_of_birth: string;
+  city_of_birth: string;
+  country_of_citizenship: string;
+  date_of_birth: string;
+  sub_position: string;
+  position: string;
+  foot: string;
+  height_in_cm: number;
+  highest_market_value_in_eur: number;
+  agent_name: string;
+  image_url: string;
+  url: string;
+  current_club_domestic_competition_id: string;
+  current_club_name: string;
+}
 
-  fetchPlayerDetails();
+export default async function Home() {
+  let players: any;
+  players = await fetchPlayerDetails();
+  console.log(players);
   return (
     <main>
-      <h1>
-        Welcome!!
-      </h1>
+      <h1>Welcome!!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Club Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player: Player, index: number) => (
+            <tr key={player.player_id}>
+              <td>{player.name}</td>
+              <td>{player.position}</td>
+              <td>{player.current_club_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
-  )
+  );
 }
--- lib/actions/player.actions.ts
+++ lib/actions/player.actions.ts
@@ -1,33 +1,18 @@
 "use server";
 
 import Player from "../models/player.model"; // Import your Player model
-import mongoose from "mongoose";
 import { connectToDB } from "../mongoose";
 
 export async function fetchPlayerDetails() {
   try {
-    connectToDB(); // Connect to the database
+    await connectToDB();
 
-    const topPlayers = await Player.find()
-      .sort({ highest_market_value_in_eur: -1 }) // Sort by market value in descending order
-      .limit(5); // Limit the result to the top 5 players
-
-    if (topPlayers.length > 0) {
-      console.log("Top 5 Players by Market Value:");
-      topPlayers.forEach((player, index) => {
-        console.log(
-          `#${index + 1}: ${player.name}, Market Value: €${
-            player.highest_market_value_in_eur
-          }`
-        );
-      });
-    } else {
-        console.log("No players exist");
-    }
+    const topPlayers = Player.find()
+      .sort({ highest_market_value_in_eur: -1 })
+      .limit(50); // Limit the result to the top 5 players
+      
+    return topPlayers;
   } catch (error) {
     console.error("Error fetching player details:", error);
   }
 }
-
-// Call the function to fetch and print player details
-// fetchPlayerDetails(598);
